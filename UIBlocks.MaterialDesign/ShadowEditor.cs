cb9f58719ac18be17b06a4ac28a410992cdba508
basic60 <basic60@outlook.com>
2016-11-11 23:41:01 +0800

    Update

---
+++ UIBlocks.MaterialDesign/ShadowEditor.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+
+namespace UIBlocks.MaterialDesign
+{
+    public enum ShadowDepth
+    {
+        Depth0,
+        Depth1,
+        Depth2,
+        Depth3,
+        Depth4,
+        Depth5
+    }
+
+    //用于存储原来的阴影信息，以便做动画变换
+    internal class ShadowInfo
+    {
+        public double ShadowOpacity { get; }
+        public ShadowInfo(double x)
+        {
+            ShadowOpacity = x;
+        }
+    }
+      
+    public static class ShadowEditor
+    {
+        public static ShadowDepth GetShadowDepth(DependencyObject obj)
+        {
+            return (ShadowDepth)obj.GetValue(ShadowDepthProperty);
+        }
+        public static void SetShadowDepth(DependencyObject obj, ShadowDepth value)
+        {
+            obj.SetValue(ShadowDepthProperty, value);
+        }
+        /// <summary>
+        /// 附加属性，设置阴影深度
+        /// </summary>
+        public static readonly DependencyProperty ShadowDepthProperty =
+            DependencyProperty.RegisterAttached("ShadowDepth", typeof(ShadowDepth), typeof(ShadowEditor), new FrameworkPropertyMetadata(default(ShadowDepth), FrameworkPropertyMetadataOptions.AffectsRender));
+
+
+        public static bool GetDarken(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(DarkenProperty);
+        }
+        public static void SetDarken(DependencyObject obj, bool value)
+        {
+            obj.SetValue(DarkenProperty, value);
+        }
+        public static readonly DependencyProperty DarkenProperty =
+            DependencyProperty.RegisterAttached("Darken", typeof(bool), typeof(ShadowEditor), new FrameworkPropertyMetadata(default(bool), FrameworkPropertyMetadataOptions.AffectsRender, DarkenPropertyChange));
+        //Darken属性发生变化是的动画
+        private static void DarkenPropertyChange(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            var uielement = obj as UIElement;
+            var dropshapdoweffect = uielement?.Effect as DropShadowEffect;
+            if (dropshapdoweffect == null) return;
+            if ((bool)e.NewValue)
+            {
+                SetLocalInfo(obj, new ShadowInfo(dropshapdoweffect.Opacity));
+                var doubleAnimation = new DoubleAnimation(1, new Duration(TimeSpan.FromMilliseconds(350)))
+                {
+                    FillBehavior = FillBehavior.HoldEnd
+                };
+                dropshapdoweffect.BeginAnimation(DropShadowEffect.OpacityProperty, doubleAnimation);
+            }
+            else
+            {
+                var slocal = GetLocalInfo(obj);
+                if (slocal == null) return;
+                var doubleAnimation = new DoubleAnimation(slocal.ShadowOpacity, new Duration(TimeSpan.FromMilliseconds(350)))
+                {
+                    FillBehavior = FillBehavior.HoldEnd
+                };
+                dropshapdoweffect.BeginAnimation(DropShadowEffect.OpacityProperty, doubleAnimation);
+            }
+        }
+
+
+
+
+        private static ShadowInfo GetLocalInfo(DependencyObject obj)
+        {
+            return (ShadowInfo)obj.GetValue(LocalInfoPropertyKey.DependencyProperty);
+        }
+
+        private static void SetLocalInfo(DependencyObject obj, ShadowInfo value)
+        {
+            obj.SetValue(LocalInfoPropertyKey, value);
+        }
+        /// <summary>
+        /// 附加属性，用于记录当前阴影透明度。
+        /// </summary>
+        private static readonly DependencyPropertyKey LocalInfoPropertyKey =
+            DependencyProperty.RegisterAttachedReadOnly("LocalInfo", typeof(ShadowInfo), typeof(ShadowEditor), new PropertyMetadata(default(ShadowInfo)));
+            
+
+    }
+}

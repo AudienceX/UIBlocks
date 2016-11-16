7fd3c45623e3aa12ab7fe1644515c473da58202b
basic60 <basic60@outlook.com>
2016-11-16 21:35:27 +0800

    日更

---
+++ UIBlocks.MaterialDesign/Card.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace UIBlocks.MaterialDesign
+{
+    public class Card:ContentControl
+    {
+        private Border clipborder;
+        static Card()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Card), new FrameworkPropertyMetadata(typeof(Card)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            clipborder = Template.FindName("ClipBorder", this) as Border;
+            Console.WriteLine(clipborder.ToString());
+        }
+
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+            base.OnRenderSizeChanged(sizeInfo);
+            if (clipborder == null) return;
+
+            var farPoint = new Point(
+                Math.Max(0, clipborder.ActualWidth),
+                Math.Max(0, clipborder.ActualHeight));
+        
+            var clipRect = new Rect(
+                new Point(),
+                new Point(farPoint.X, farPoint.Y));
+
+            ClipContent = new RectangleGeometry(clipRect, Cornerradius, Cornerradius);
+        }
+
+        #region 依赖属性CornerRadius，卡片圆角
+        public double Cornerradius
+        {
+            get { return (double)GetValue(CornerradiusProperty); }
+            set { SetValue(CornerradiusProperty, value); }
+        }
+        public static readonly DependencyProperty CornerradiusProperty =
+            DependencyProperty.RegisterAttached("Cornerradius", typeof(double), typeof(Card), new FrameworkPropertyMetadata(2.0, FrameworkPropertyMetadataOptions.AffectsMeasure));
+        #endregion
+
+        #region ClipContent依赖属性，卡牌内容形状
+        public Geometry ClipContent
+        {
+            get { return (Geometry)GetValue(ClipContentProperty); }
+            set { SetValue(ClipContentProperty, value); }
+        }
+        public static readonly DependencyProperty ClipContentProperty =
+            DependencyProperty.RegisterAttached("ClipContent", typeof(Geometry), typeof(Card), new PropertyMetadata(default(Geometry)));
+        #endregion
+
+    }
+}
+    
---
+++ UIBlocks.MaterialDesign/Converters/CardClipConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace UIBlocks.MaterialDesign.Converters
+{
+    public class CardClipConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2 || !(values[0] is Size) || !(values[1] is Thickness))
+                return Binding.DoNothing;
+
+            var size = (Size)values[0];
+            var farPoint = new Point(
+                Math.Max(0, size.Width),
+                Math.Max(0, size.Height));
+            var padding = (Thickness)values[1];
+            farPoint.Offset(padding.Left + padding.Right, padding.Top + padding.Bottom);
+
+            return new Rect(
+                new Point(),
+                new Point(farPoint.X, farPoint.Y));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

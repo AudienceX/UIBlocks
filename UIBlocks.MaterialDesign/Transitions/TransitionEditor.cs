9fb7ac472153c8d9325180d6134961b0ad3168c2
basic60 <basic60@outlook.com>
2016-12-05 20:27:19 +0800

    update

---
+++ UIBlocks.MaterialDesign/Transitions/TransitionEditor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UIBlocks.MaterialDesign.Transitions
+{
+    public static class TransitionEditor
+    {
+        /// <summary>
+        /// 附加属性，是否开启变换。
+        /// </summary>
+        public static readonly DependencyProperty DisableTransitionsProperty = DependencyProperty.RegisterAttached(
+    "DisableTransitions", typeof(bool), typeof(TransitionEditor), new FrameworkPropertyMetadata(default(bool), FrameworkPropertyMetadataOptions.Inherits));
+
+        public static void SetDisableTransitions(DependencyObject element, bool value)
+        {
+            element.SetValue(DisableTransitionsProperty, value);
+        }
+        public static bool GetDisableTransitions(DependencyObject element)
+        {
+            return (bool)element.GetValue(DisableTransitionsProperty);
+        }
+    }
+}

aaa0bf1f4974bbb36233dc1e9fdaed0a4461b786
basic60 <basic60@outlook.com>
2017-02-23 16:21:24 +0800

    update

---
+++ UIBlocks.MaterialDesignTheme/PackIcon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UIBlocks.MaterialDesignTheme
+{
+    public class PackIcon : PackIconBase<PackIconKind>
+    {
+        static PackIcon()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PackIcon), new FrameworkPropertyMetadata(typeof(PackIcon)));
+        }
+
+        public PackIcon() : base(PackIconDataFactory.Create) { }
+    }
+}
---
+++ UIBlocks.MaterialDesignTheme/ToolTipEditor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace UIBlocks.MaterialDesignTheme
+{
+    public static class ToolTipEditor
+    {
+        public static CustomPopupPlacementCallback CustomPopupPlacementCallback => CustomPopupPlacementCallbackImpl;
+
+        public static CustomPopupPlacement[] CustomPopupPlacementCallbackImpl(Size popupSize, Size targetSize, Point offset)
+        {
+            return new[]
+            {
+                new CustomPopupPlacement(new Point(targetSize.Width/2 - popupSize.Width/2, targetSize.Height + 14), PopupPrimaryAxis.Horizontal)
+            };
+        }
+    }
+}
